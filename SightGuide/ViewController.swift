c96fad59a4869e4286c5ded947516ffab18712fe
khushiverma66 <khushi1742.be21@chitkara.edu.in>
2024-04-25 10:06:03 +0530

    initial commit

---
+++ SightGuide/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SightGuide
+//
+//  Created by Student on 08/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var holdButton: UILongPressGestureRecognizer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+                                                                                                                                                                                 
